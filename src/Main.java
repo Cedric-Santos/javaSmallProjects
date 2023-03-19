adbb9fefde3bc1cb6af65343d212d858393656d7
Cedric Santos <cedricdevcareer@outlook.com>
2023-03-19 11:16:07 -0300

    Adding the first project's classes, along with some basic structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+
+        System.out.println("Insert new address");
+        cliente.setEndereco(scanner.next());
+        System.out.println(cliente.getEndereco());
     }
 }
