3e34215e1c56b06f709eb0bd005a8dbcae2faee9
Cedric Santos <cedricdevcareer@outlook.com>
2023-03-19 13:21:10 -0300

    Re-writing some classes, Instantiating all the subclasses into the Main class, and also starting the main code structure.

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -4,4 +4,27 @@ public class Funcionario {
 
     private String nome, endereco;
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
 public class Main {
+    private int decision;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Cliente cliente = new Cliente();
+        Scanner     scanner     = new Scanner(System.in);
+        Cliente     cliente     = new Cliente();
+        Funcionario funcionario = new Funcionario();
+        Produto     produto     = new Produto();
+        Venda       venda       = new Venda();
+        ItemVenda   itemVenda   = new ItemVenda();
 
-        System.out.println("Insert new address");
-        cliente.setEndereco(scanner.next());
-        System.out.println(cliente.getEndereco());
+
+
+        System.out.println("\nBem vindo ao sistema logístico.\nO quê deseja fazer?");
+        System.out.println("\n1 - Registrar Cliente" +
+                           "\n2 - Registrar Funcionario" +
+                           "\n3 - Registrar Produto" +
+                           "\n4 - Checar Produtos Disponíveis" +
+                           "\n5 - Relatório de Vendas" +
+                           "\n6 - Sair");
+
     }
 }
