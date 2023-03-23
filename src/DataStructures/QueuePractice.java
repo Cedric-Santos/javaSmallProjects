7dc406d2d146269a05b724cf7ff0a9a1183417b3
Cedric Santos <cedricdevcareer@outlook.com>
2023-03-23 09:59:38 -0300

    Adding new Data Structures' practicing class "QueuePractice"

---
+++ src/DataStructures/QueuePractice.java
@@ -0,0 +1,13 @@
+package DataStructures;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class QueuePractice {
+
+    Scanner s = new Scanner(System.in);
+    Queue<String> queue = new LinkedList<String>();
+
+
+}
