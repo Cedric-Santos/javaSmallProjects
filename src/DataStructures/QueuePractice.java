92dee581ddf8f8adc312ead9aad4271659f79281
Cedric Santos <cedricdevcareer@outlook.com>
2023-03-24 12:00:13 -0300

    Partially implementation of my queue practicing exercise.

--- src/DataStructures/QueuePractice.java
+++ src/DataStructures/QueuePractice.java
@@ -4,10 +4,42 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+
 public class QueuePractice {
 
     Scanner s = new Scanner(System.in);
-    Queue<String> queue = new LinkedList<String>();
+    static Queue<String> queue = new LinkedList<String>();
+
+    public static void addPDF (String[] pdf) {
+        for(Integer i = 0; i < pdf.length; i++) {
+            queue.offer(pdf[i]);
+        }
+    }
+
+    public static void removePDF (String[] pdf) {
+        while (!queue.isEmpty()) {
+            queue.poll();
+        }
+    }
+
+    public static void main (String args[]) {
+
+        String[] cars = {"Volvo", "BMW", "Audi", "Toyota", "Volkswagen"};
+
+        addPDF(cars);
+
+        System.out.println(queue);
+
+        removePDF(cars);
+
+        System.out.println(queue.isEmpty());
+
+    }
+
+
+
+
+//Offer(), Poll(), Peek()
 
 
 }
